66f562e323dd22a232e4ed86ae2f1cbaa57e9033
AceAlBastoni <elhlawy@gmail.com>
2024-08-06 18:41:39 +0300

    AceAlBastoni.Sync_to_gdrive.Prod creating temp before send to remote

--- AceAlBastoni.Sync_to_gdrive.Prod.bat
+++ AceAlBastoni.Sync_to_gdrive.Prod.bat
@@ -1,4 +1,8 @@
 @echo off
+
+REM Set code page to UTF-8
+chcp 65001 >nul
+
 :loop
 @echo ██████████████████████████████████████████████████████████████████████████████████████████████████████████████████
 @echo start AceAlbastoni.
@@ -9,25 +13,44 @@ SET RCLONE_PATH=C:\rclone\rclone-v1.66.0-windows-amd64\rclone.exe
 SET REMOTE_NAME=drive
 SET FOLDER_ID=LinkedIn
 SET LOCAL_PATH=C:\rclone\rclone-v1.66.0-windows-amd64\New folder
+SET TEMP_PATH=C:\rclone\rclone-v1.66.0-windows-amd64\Temp
+SET LOG_FILE=C:\rclone\rclone-v1.66.0-windows-amd64\logfile.txt
 
-REM Copy files to remote
-echo Copying to %REMOTE_NAME%:%FOLDER_ID%
-%RCLONE_PATH% copy "%LOCAL_PATH%" %REMOTE_NAME%:%FOLDER_ID% --ignore-existing --include "*█*"
+REM Create temporary folder if it doesn't exist
+if not exist "%TEMP_PATH%" mkdir "%TEMP_PATH%"
 
-REM Add a short delay to ensure all operations are completed
-timeout /t 5 /nobreak >nul
+REM Move files to temporary folder
+for %%f in ("%LOCAL_PATH%\*█*") do (
+    move "%%f" "%TEMP_PATH%\"
+    if not errorlevel 1 (
+        echo Moved to temp: %%~ff
+        echo Moved to temp: %%~ff >> "%LOG_FILE%"
+    ) else (
+        echo Error moving %%~ff to temp
+        echo Error moving %%~ff to temp >> "%LOG_FILE%"
+    )
+)
 
-REM Delete files based on age
-powershell -Command "Get-ChildItem -Path '%LOCAL_PATH%' -Filter '*█*' | Where-Object { $_.LastWriteTime -lt (Get-Date).AddSeconds(-50) } | Remove-Item -Force"
-powershell -Command "Get-ChildItem -Path '%LOCAL_PATH%' -Filter '*█*' | Where-Object { $_.LastWriteTime -lt (Get-Date).AddMinutes(-1) } | Remove-Item -Force"
-powershell -Command "Get-ChildItem -Path '%LOCAL_PATH%' -Filter '*█*' | Where-Object { $_.LastWriteTime -lt (Get-Date).AddHours(-1) } | Remove-Item -Force"
-powershell -Command "Get-ChildItem -Path '%LOCAL_PATH%' -Filter '*█*' | Where-Object { $_.LastWriteTime -lt (Get-Date).AddDays(-1) } | Remove-Item -Force"
-powershell -Command "Get-ChildItem -Path '%LOCAL_PATH%' -Filter '*█*' | Where-Object { $_.LastWriteTime -lt (Get-Date).AddMonths(-1) } | Remove-Item -Force"
-powershell -Command "Get-ChildItem -Path '%LOCAL_PATH%' -Filter '*█*' | Where-Object { $_.LastWriteTime -lt (Get-Date).AddYears(-1) } | Remove-Item -Force"
+REM Move files from temporary folder to remote
+for %%f in ("%TEMP_PATH%\*█*") do (
+    echo Moving %%~ff to %REMOTE_NAME%:%FOLDER_ID%
+    %RCLONE_PATH% move "%%f" %REMOTE_NAME%:%FOLDER_ID%
+    if not errorlevel 1 (
+        echo Moved: %%~ff to %REMOTE_NAME%:%FOLDER_ID%
+        echo Moved: %%~ff to %REMOTE_NAME%:%FOLDER_ID% >> "%LOG_FILE%"
+    ) else (
+        echo Error moving %%~ff to %REMOTE_NAME%:%FOLDER_ID%
+        echo Error moving %%~ff to %REMOTE_NAME%:%FOLDER_ID% >> "%LOG_FILE%"
+    )
+    timeout /t 1 /nobreak >nul
+)
 
 REM Refresh the folder by listing its contents
 echo Current contents of %LOCAL_PATH%:
 dir "%LOCAL_PATH%"
+
+echo Current contents of %TEMP_PATH%:
+dir "%TEMP_PATH%"
 
 REM Pause to see the result (optional)
 :: pause
